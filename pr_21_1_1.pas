66e0f453c4da6a05f4025371826124784a5956fe
ivansk201 <skeletonlive@mail.ru>
2024-01-19 14:23:04 +0500

    Фигма

---
+++ pr_21_1_1.pas
@@ -0,0 +1,15 @@
+﻿program oneandone;
+var 
+  a,b,s,p:real;
+  begin
+    try
+  begin
+    Writeln('Введите длину двух катетов');
+    Readln(a, b);
+    s:= Round((a * b) / 2);
+    p:= Round((sqrt(sqr(a)+ sqr(b)) + a + b));
+    writeln('Пощадь - ' + s +' Периметр - ' + p);
+    end;
+    except writeln('Введено неверно');
+  end;
+  end.
